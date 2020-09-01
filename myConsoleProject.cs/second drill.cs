cd152df9088b7f8cf8a7f14d09cdaf96474728cf
Ryan <zelda0169@gmailcom>
2020-09-01 16:40:08 -0600

    Add project files.

---
+++ myConsoleProject.cs/second drill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to acme Accounting Sytems \n Remember, we're\"accounting\"on you!");
+        Console.ReadLine();
+    }
+}
+
