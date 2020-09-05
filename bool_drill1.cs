13fb3ee3f5df4f3439b6deeafd6bee48883a68b7
Ryan <zelda0169@gmailcom>
2020-09-05 09:06:48 -0600

    video go along and first bool drill

---
+++ bool_drill1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bool_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string Dui = (Console.ReadLine());
+            bool DuiTest = false;
+            if (Dui == "true" || Dui == "yes")
+            {
+                DuiTest = true;
+            }
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tic = Convert.ToInt32(Console.ReadLine());
+            bool CD = (age > 15 && DuiTest == true && tic < 3);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(CD);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ boolvideo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+ususing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean Logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //Console.WriteLine(true && false);
+        //Console.WriteLine(true && true);
+        //Console.WriteLine(false && false);
+
+        //Console.WriteLine(true || true);
+        //Console.WriteLine(true || false);
+        //Console.WriteLine(false || false);
+
+        //Console.WriteLine(true == true);
+        //Console.WriteLine(true == false);
+        //Console.WriteLine(false == false);
+
+        //Console.WriteLine(true != true);
+        //Console.WriteLine(true != false);
+        //Console.WriteLine(false!= false);
+
+        //Console.WriteLine(true ^ true);
+        //Console.WriteLine(true ^ false);
+        //Console.WriteLine(false ^ false);
+
+        Console.ReadLine();
+        }
+    }
+}
