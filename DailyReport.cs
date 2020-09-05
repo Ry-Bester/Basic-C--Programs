0b433f8eeac9b42c04e0d0b0c77edd39fa855706
Ryan <zelda0169@gmailcom>
2020-09-05 08:45:05 -0600

    fixed issue with daily report

---
+++ DailyReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dailyreport_drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine(" What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            int pgnum = Convert.ToInt32(Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer 'True' or 'False'. ");
+            string help = Console.ReadLine();
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give Specifics");
+            string posEx = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be Specific");
+            string feedBa = Console.ReadLine();
+            Console.WriteLine("How Many Hours did you study today?");
+            string hoursStudied = Console.ReadLine();
+            int hrs = Convert.ToInt32(hoursStudied);
+            Console.WriteLine("Thank you for your answers! An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+        }
+    }
+}
